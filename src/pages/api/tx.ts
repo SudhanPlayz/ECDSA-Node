4cba4f39a064fe623a0f6874ad447554310dde52
SudhanPlayz <sudhanplayz@gmail.com>
2023-12-28 12:41:09 +0000

    Refactor API endpoint to use "tx" instead of "id" as query parameter

--- src/pages/api/tx.ts
+++ src/pages/api/tx.ts
@@ -6,17 +6,17 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const { id } = req.query;
+    const { tx } = req.query;
 
-    if (!id) return res.status(400).send({ error: 'No address specified' });
+    if (!tx) return res.status(400).send({ error: 'No address specified' });
     
-    const tx = await db.transaction.findUnique({
+    const TX = await db.transaction.findUnique({
         where: {
-            id: id as string
+            id: tx as string
         }
     })
 
-    if (!tx) return res.status(404).send({ error: 'Transaction not found' });
+    if (!TX) return res.status(404).send({ error: 'Transaction not found' });
 
-    return res.status(200).send(tx)
+    return res.status(200).send(TX)
 }
