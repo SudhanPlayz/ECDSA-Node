b52d218ea4ac9c904b5e273c542c13e83c9e6e8e
SudhanPlayz <sudhanplayz@gmail.com>
2023-12-29 12:00:06 +0000

    Update author name in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="flex flex-row justify-center items-center bg-slate-900 h-16 w-screen">
-            <p className="text-white text-lg">Made by <a href="https://github.com/SudhanPlayz" className="text-blue-500 hover:underline">SudhanPlayz</a></p>
+            <p className="text-white text-lg">Made by <a href="https://github.com/SudhanPlayz" className="text-blue-500 hover:underline">Sudhan</a></p>
         </footer>
     )
 }
